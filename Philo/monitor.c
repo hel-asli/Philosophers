5db7fa4a61ed32794c7494e405836d5c184fe51b
Hamza El Asli <hel-asli@e3r4p14.1337.ma>
2024-11-25 01:02:43 +0100

    ...

--- Philo/monitor.c
+++ Philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: hel-asli <hel-asli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 23:23:28 by hel-asli          #+#    #+#             */
-/*   Updated: 2024/11/25 00:16:17 by hel-asli         ###   ########.fr       */
+/*   Updated: 2024/11/25 00:55:21 by hel-asli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,7 @@ void	*monitor_job(void *arg)
 			nb_meals_check(&philo[i]);
 			i++;
 		}
-		usleep(100);
+		usleep(300);
 	}
 	return (NULL);
 }
